11a6be984a01c5c67be0bc2fbd4bc081f2a4f801
saman-kh <66360401+saman-kh@users.noreply.github.com>
2020-12-16 09:20:14 +0330

    img directory
    for showing ubject on page

--- Dice Game/gameCode.js
+++ Dice Game/gameCode.js
@@ -3,13 +3,13 @@
 function randDice(){
 var randNum1 = Math.floor(Math.random()*6) + 1;
 var randDiceImg = "Dice"+randNum1+".png";
-var randImgSrc1 = "image/"+randDiceImg;
+var randImgSrc1 = "/image/"+randDiceImg;
 var img1 = document.querySelectorAll("img")[0];
 img1.setAttribute("src",randImgSrc1);
 
 var randNum2 = Math.floor(Math.random()*6 )+1;
 var randDiceImg2 = "Dice"+randNum2+".png";
-var randImgSrc2 = "image/"+randDiceImg2;
+var randImgSrc2 = "/image/"+randDiceImg2;
 var img2 = document.querySelectorAll("img")[1];
 img2.setAttribute("src",randImgSrc2);
 
